b8e5fa8107a28fce90a69a0799279da5ed712f4b
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2019-04-09 11:24:34 -0300

    adding categories and some validations

--- app/models/studant.rb
+++ app/models/studant.rb
@@ -1,3 +1,10 @@
 class Studant < ApplicationRecord
+  #Associations
   belongs_to :project
+
+  #Validations
+  validates :name, :email
+
+  enum category:[:scientific_research,:masters_degree,:doctorate_degree,:post_doctoral]
+
 end
