eba582dc106c1759c160f6ae8ec8cfdc07facbed
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2019-04-12 14:37:05 -0300

    Improving syntax

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
+  extend Devise::Models
   self.abstract_class = true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class User < ActiveRecord::Base
-  extend Devise::Models
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
