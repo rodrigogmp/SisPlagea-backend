aa07f352d283ecab9a34f7b34236670522094178
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2019-06-17 21:33:28 -0300

    publications CRUD working

--- app/controllers/api/v1/publications_controller.rb
+++ app/controllers/api/v1/publications_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PublicationsController < Api::V1::BaseController
-
+before_action :set_publication, only:[:show,:destroy,:update]
   def create
     @publication = Publication.new params_publication
 
@@ -8,8 +8,33 @@ class Api::V1::PublicationsController < Api::V1::BaseController
         end
   end
 
+  def index
+    @publications = Publication.all
+  end
+
+  def show
+  end
+
+  def destroy
+    unless @publication.destroy
+        render json: {errors: @publication.errors.full_messages}, status: :bad_request
+    else
+        render json: {message: "Publicação excluida com sucesso"}, status: :ok
+    end
+  end
+
+  def update
+    unless @publication.update params_publication
+      render json: {errors: @publication.errors.full_messages}, status: :bad_request
+    end
+  end
+
   private
     def params_publication
       params.permit(:title,:journal,:category,:year,:volume,:pages,:authors,:file_to_upload)
+    end
+
+    def set_publication
+      @publication = Publication.find(params[:id])
     end
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,7 +1,7 @@
 class Publication < ApplicationRecord
     
     #Associations
-    has_one :attachment, dependent: :destroy
+    # has_one :attachment, dependent: :destroy
     validates :title,:journal,:category,:year,:volume,:pages,:authors, presence: true
 
     enum category:[:conference,:abstract,:periodic]
