b300b60cc24e3c713c5a80b27bea55d16d508969
Rodrigo Marafelli <rodrigomarafelli@ioasys.com.br>
2019-06-29 16:48:11 -0300

    creating endpoint to create a publication using a bibtex file

--- app/controllers/api/v1/publications_controller.rb
+++ app/controllers/api/v1/publications_controller.rb
@@ -8,17 +8,43 @@ class Api::V1::PublicationsController < Api::V1::BaseController
             render json: {errors: @publication.errors.full_messages}, status: :bad_request
         end
   end
-  # require 'bibtex'
+  
   def create_from_bibtex
-    require 'bibtex'
     @bibtex = Bibtex.new params_create_from_bibtex
-    
     unless @bibtex.save
       render json: {errors: "Erro ao fazer upload de arquivo bibtex"}, status: :bad_request
     else
       length = @bibtex.file.path.length
-      path = @bibtex.file.path[31,length]
-      b = BibTeX.open(`.#{path}`)
+      path = '.'+@bibtex.file.path[31,length]
+      b = BibTeX.open(path)
+      if b.first.type.to_s == 'conference'
+        @publication = Publication.new(title: b.first.title.value,
+                                      journal: b.first.journal.value,
+                                      category: b.first.type.to_s,
+                                      year: b.first.year.value,
+                                      volume: b.first.volume.value,
+                                      pages: b.first.pages.value,
+                                      authors: b.first.author.value)
+      else
+        if b.first.type.to_s == 'inproceedings'
+          @publication = Publication.new(title: b.first.title.value,
+                                         year: b.first.year.value,
+                                         category: 'abstract',
+                                         authors: b.first.author.value,
+                                         volume: b.first.volume.value)
+        else
+          if b.first.type.to_s == 'article'
+            @publication = Publication.new(title: b.first.title.value,
+                                           year: b.first.year.value,
+                                           category: 'periodic',
+                                           authors: b.first.author.value,
+                                           volume: b.first.volume.value)
+          end
+        end
+      end
+      unless @publication.save
+        render json: {errors: @publication.errors.full_messages}, status: :bad_request
+      end
     end
 
   end
